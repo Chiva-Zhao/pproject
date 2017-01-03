6f24aba008bdcbe4e3344a3e0535bc7ae9731355
Chiva-Zhao <zhaozhenhua88@gmail.com>
2017-01-03 13:22:29 +0800

    json to panda dataframe

---
+++ crawl/JsonPanda.py
@@ -0,0 +1,21 @@
+import json
+import urllib.request
+import types
+import pandas as pd
+from pandas import Series, DataFrame
+from pandas.io.json import json_normalize
+
+url = 'https://rong.36kr.com/api/organization/investor?page='
+req = urllib.request.Request(url)
+response = urllib.request.urlopen(req)
+data = response.read()
+data = data.decode()
+myjson = json.loads(data)
+print(myjson)
+# users = myjson['data']['data'][0]['user']
+users = json_normalize(myjson['data']['data'])
+# siblings = DataFrame(users,columns=['name','intro'])
+# print(users['user.name'])
+# print(users['user.intro'])
+df = DataFrame(users, columns=['user.name', 'user.intro'])
+print(df)
