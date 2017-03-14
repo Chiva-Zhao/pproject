bf2a2a728978f81b599ab91fba5316bd20819e8c
Chiva-Zhao <zhaozhenhua88@gmail.com>
2017-03-14 22:42:24 +0800

    heapq模块应用和实现优先队列

---
+++ cookbook/PriorityQueue.py
@@ -0,0 +1,39 @@
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'item({!r})'.format(self.name)
+
+
+q = PriorityQueue()
+q.push(Item('foo'), 1)
+q.push(Item('bar'), 5)
+q.push(Item('spam'), 4)
+q.push(Item('grok'), 1)
+print(q.pop())
+print(q.pop())
+print(q.pop())
+print(q.pop())
+
+a=(1,Item('foo'))
+b=(2,Item('bar'))
+c=(1,Item('grok'))
+print(a<b)
+print(a<c)
---
+++ cookbook/max_min_n.py
@@ -0,0 +1,23 @@
+import heapq
+
+nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+print(heapq.nlargest(3, nums))  # Prints [42, 37, 23]
+print(heapq.nsmallest(3, nums))
+
+portfolio = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+cheap = heapq.nsmallest(3, portfolio, lambda s: s['price'])
+expensive = heapq.nlargest(3, portfolio, lambda s: s['price'])
+# print(cheap, expensive)
+
+heapq.heapify(nums)
+print(nums)
+print(heapq.heappop(nums))
+print(heapq.heappop(nums))
+print(heapq.heappop(nums))
