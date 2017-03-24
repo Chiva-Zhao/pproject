e35e33ec00b06b359b31187c62d39e013ec1bab1
Chiva-Zhao <zhaozhenhua88@gmail.com>
2017-03-24 23:23:50 +0800

    忽略大小写匹配和非捕获分组

--- cookbook/textoperate/replace.py
+++ cookbook/textoperate/replace.py
@@ -1,14 +1,14 @@
+import re
+
+datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
+text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
+
+
 # 你想在字符串中搜索和替换指定的文本模式
 def simple():
     text = 'yeah, but no, but yeah, but no, but yeah'
     replaced = text.replace('yeah', 'yap')
     print(text, replaced)
-
-
-import re
-
-datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
-text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
 
 
 # 对于复杂的模式，请使用 re 模块中的 sub() 函数。为了说明这个，假设你想将形
@@ -27,5 +27,31 @@ def re_more(m):
     mon_name = month_abbr[int(m.group(1))]
     return '{} {} {}'.format(m.group(2), mon_name, m.group(3))
 
-re_match()
-print(datepat.sub(re_more, text))
+
+def matchcase(word):
+    def replace(m):
+        text = m.group()
+        if text.isupper():
+            return word.upper()
+        elif text.islower():
+            return word.lower()
+        elif text[0].isupper():
+            return word.capitalize()
+        else:
+            return word
+
+    return replace
+
+
+def ignoreCase():
+    text = 'UPPER PYTHON, lower python, Mixed Python'
+    print(re.findall("python", text, flags=re.IGNORECASE))
+    print(re.sub('python', 'snake', text, flags=re.IGNORECASE))
+    # 最后的那个例子揭示了一个小缺陷，替换字符串并不会自动跟被匹配字符串的大小
+    # 写保持一致。为了修复这个，你可能需要一个辅助函数
+    print(re.sub('python', matchcase('snake'), text, flags=re.IGNORECASE))
+
+
+# re_match()
+# print(datepat.sub(re_more, text))
+ignoreCase()
