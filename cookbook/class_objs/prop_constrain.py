ed6bcb00515678d5724a8dd7ed8f0ac67b5ccb30
Chiva-Zhao <zhaozhenhua88@gmail.com>
2017-06-01 23:25:44 +0800

    实现数据模型的类型约束

---
+++ cookbook/class_objs/prop_constrain.py
@@ -0,0 +1,86 @@
+# 实现数据模型的类型约束
+# 在这个问题中，你需要在对某些实例属性赋值时进行检查。所以你要自定义属性赋
+# 值函数，这种情况下最好使用描述器。
+# 下面的代码使用描述器实现了一个系统类型和赋值验证框架：
+# Base class. Uses a descriptor to set a value
+class Descriptor:
+    def __init__(self, name=None, **opts):
+        self.name = name
+        for key, value in opts.items():
+            setattr(self, key, value)
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+
+# Descriptor for enforcing types
+class Typed(Descriptor):
+    expected_type = type(None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError('expected ' + str(self.expected_type))
+        super().__set__(instance, value)
+
+
+# Descriptor for enforcing values
+class Unsigned(Descriptor):
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError('Expected >= 0')
+        super().__set__(instance, value)
+
+
+class MaxSized(Descriptor):
+    def __init__(self, name=None, **opts):
+        if 'size' not in opts:
+            raise TypeError('missing size option')
+        super().__init__(name, **opts)
+
+    def __set__(self, instance, value):
+        if len(value) >= self.size:
+            raise ValueError('size must be < ' + str(self.size))
+        super().__set__(instance, value)
+
+
+# 这些类就是你要创建的数据模型或类型系统的基础构建模块。下面就是我们实际定
+# 义的各种不同的数据类型：
+class Integer(Typed):
+    expected_type = int
+
+
+class UnsignedInteger(Integer, Unsigned):
+    pass
+
+
+class Float(Typed):
+    expected_type = float
+
+
+class UnsignedFloat(Float, Unsigned):
+    pass
+
+
+class String(Typed):
+    expected_type = str
+
+
+class SizedString(String, MaxSized):
+    pass
+
+
+# 然后使用这些自定义数据类型，我们定义一个类：
+class Stock:
+    # Specify constraints
+    name = SizedString('name', size=8)
+    shares = UnsignedInteger('shares')
+    price = UnsignedFloat('price')
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+
+s = Stock()
+s.name
