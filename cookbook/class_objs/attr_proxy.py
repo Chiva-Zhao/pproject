26806f1499df8f32513eac9005ae33043d34e3bc
zhongdaxin <zzh@daxinpj.com>
2017-06-07 17:57:32 +0800

    属性的代理访问

--- cookbook/class_objs/attr_proxy.py
+++ cookbook/class_objs/attr_proxy.py
@@ -98,5 +98,98 @@ p = Proxy(s)
 print(p.x)  # Outputs 2
 p.bar(3)  # Outputs "Spam.bar: 2 3"
 p.x = 37  # Changes s.x to 37
+
+
 # 通过自定义属性访问方法，你可以用不同方式自定义代理类行为 (比如加入日志功
 # 能、只读访问等)
+# 代理类有时候可以作为继承的替代方案。例如，一个简单的继承如下
+class A:
+    def spam(self, x):
+        print('A.spam', x)
+
+    def foo(self):
+        print('A.foo')
+
+
+class B(A):
+    def spam(self, x):
+        print('B.spam')
+        super().spam(x)
+
+    def bar(self):
+        print('B.bar')
+
+
+# 使用代理的话，就是下面这样：
+class A:
+    def spam(self, x):
+        print('A.spam', x)
+
+    def foo(self):
+        print('A.foo')
+
+
+class B:
+    def __init__(self):
+        self._a = A()
+
+    def spam(self, x):
+        print('B.spam', x)
+        self._a.spam(x)
+
+    def bar(self):
+        print('B.bar')
+
+    def __getattr__(self, name):
+        return getattr(self._a, name)
+
+
+# 当实现代理模式时，还有些细节需要注意。首先， getattr () 实际是一个后备
+# 方法，只有在属性不存在时才会调用。因此，如果代理类实例本身有这个属性的话，
+# 那么不会触发这个方法的。另外， setattr () 和 delattr () 需要额外的魔法来
+# 区分代理实例和被代理实例 obj 的属性。一个通常的约定是只代理那些不以下划线
+# 开头的属性 (代理类只暴露被代理类的公共属性)。
+# 还有一点需要注意的是， getattr () 对于大部分以双下划线 ( ) 开始和结尾的
+# 属性并不适用。比如，考虑如下的类
+class ListLike:
+    """__getattr__ 对于双下划线开始和结尾的方法是不能用的，需要一个个去重定义"""
+
+    def __init__(self):
+        self._items = []
+
+    def __getattr__(self, name):
+        return getattr(self._items, name)
+
+
+# 如果是创建一个 ListLike 对象，会发现它支持普通的列表方法，如 append() 和
+# insert()，但是却不支持 len()、元素查找等。例如
+a = ListLike()
+a.append(2)
+a.insert(0, 1)
+a.sort()
+
+
+# len(a)TypeError: object of type 'ListLike' has no len()
+# a[0] TypeError: 'ListLike' object does not support indexing
+# 为了让它支持这些方法，你必须手动的实现这些方法代理：
+class ListLike:
+    """__getattr__ 对于双下划线开始和结尾的方法是不能用的，需要一个个去重定义"""
+
+    def __init__(self):
+        self._items = []
+
+    def __getattr__(self, name):
+        return getattr(self._items, name)
+
+    # Added special methods to support certain list operations
+    def __len__(self):
+        return len(self._items)
+
+    def __getitem__(self, index):
+        return self._items[index]
+
+    def __setitem__(self, index, value):
+        self._items[index] = value
+
+    def __delitem__(self, index):
+        del self._items[index]
